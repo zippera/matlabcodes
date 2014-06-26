c67534a3c9aad4c1c707c6a11b9c75edc2a98ba3
zippera <sovey198@gmail.com>
2014-06-26 22:40:15 +0800

    update sampling

--- sampling.m
+++ sampling.m
@@ -7,11 +7,13 @@ clear;
 % date: 2014.6.26
 
 wparticles = [.1, .3, .2, .4];% 4particles with weights
+%swparticles = sort(wparticles, 'descend');% sort at first
+%wparticles = swparticles;
 lparticles = length(wparticles);% 4
 nparticles = 1000;% resample numbers
 particles = [];
 freqs = [0,0,0,0];% to count num of new particles from the correspondent old ones
-
+tic
 for i = 1 : nparticles
     rnum = rand();
     weight_sum = 0;
@@ -24,5 +26,6 @@ for i = 1 : nparticles
     particles = [particles, j];
     freqs(j) = freqs(j) + 1;
 end
+toc
 disp(freqs/nparticles);% display the approximation
 hist(particles, lparticles);% display above in hist
